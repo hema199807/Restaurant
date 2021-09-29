7964b3ac041675659fc1560fc51c1a1cad51edfe
hema199807 <d.hema257@gmail.com>
2021-09-29 14:06:23 +0530

    Updates

---
+++ precache-manifest.3db5458e3d23db0914922ea6b395ae11.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f64a31ab7c58f6f42f318af43ce66f5",
+    "url": "/Restaurant/index.html"
+  },
+  {
+    "revision": "b3e97277335887ec9599",
+    "url": "/Restaurant/static/css/2.fb230578.chunk.css"
+  },
+  {
+    "revision": "889754f0c9129c606288",
+    "url": "/Restaurant/static/css/main.77c8a1e1.chunk.css"
+  },
+  {
+    "revision": "b3e97277335887ec9599",
+    "url": "/Restaurant/static/js/2.3cd19f93.chunk.js"
+  },
+  {
+    "revision": "0ccb586afcc8a01ad897e0b6d4e55d4a",
+    "url": "/Restaurant/static/js/2.3cd19f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889754f0c9129c606288",
+    "url": "/Restaurant/static/js/main.57085e56.chunk.js"
+  },
+  {
+    "revision": "e2eb9b688eec6448e736",
+    "url": "/Restaurant/static/js/runtime-main.dbfa80fd.js"
+  },
+  {
+    "revision": "27e43d3258e6d0941c97fceb23c7b921",
+    "url": "/Restaurant/static/media/breakfast.27e43d32.jpg"
+  },
+  {
+    "revision": "44d24281ebf09a0285f90baaba945182",
+    "url": "/Restaurant/static/media/desktop.44d24281.ini"
+  },
+  {
+    "revision": "862fd5c7313efafaa9de7f3e57eaf13b",
+    "url": "/Restaurant/static/media/dinner.862fd5c7.png"
+  },
+  {
+    "revision": "b027a40218c70692d8efec6db30f8c2d",
+    "url": "/Restaurant/static/media/drinks.b027a402.png"
+  },
+  {
+    "revision": "0f1930af82fc12675e5d680a88b8fdce",
+    "url": "/Restaurant/static/media/food.0f1930af.jpg"
+  },
+  {
+    "revision": "d09dc85d8611c38a57a15d4842ebb62e",
+    "url": "/Restaurant/static/media/homepageimg.d09dc85d.png"
+  },
+  {
+    "revision": "1ea2d69dc3541adefd7d02983e1bfab2",
+    "url": "/Restaurant/static/media/lunch.1ea2d69d.jpg"
+  },
+  {
+    "revision": "aefd75ca8300b2764eac0d16bff9bbde",
+    "url": "/Restaurant/static/media/nightlife.aefd75ca.png"
+  },
+  {
+    "revision": "bbb58d6292abbfce22a4c95caaa0f417",
+    "url": "/Restaurant/static/media/snacks.bbb58d62.png"
+  }
+]);
